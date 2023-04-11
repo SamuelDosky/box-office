8b3213fa9cc37668eaac7a6b8ae2a8706847fa1d
SamuelDosky <doskysamuel@gmail.com>
2023-04-11 18:38:00 -0400

    add movie search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Starred from './pages/Starred'
-import Navs from './components/Navs'
+// import Navs from './components/Navs'
 import MainLayout from "./components/MainLayout"
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<MainLayout />}>
-            <Route path="/" element={<Navs />}/>
-            <Route path="/home" element={<Home />} />
+            {/* <Route path="/" element={<Navs />}/> */}
+            <Route path="/" element={<Home />} />
             <Route path="/starred" element={<Starred />} />
             <Route path="*" element={<div> Not Found </div>} />
           </Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react';
+import axios from 'axios'
 
 const Home = () => {
+
+    const [search , setSearch] = useState("");
+
+    const handleChange = (e) => {
+       setSearch(e.target.value)
+    }
+
+    const handleSearch = () => {
+        axios.get(`https://api.tvmaze.com/search/shows?q =${search}`)
+        .then(response => console.log(response.data))
+    }
+
     return(
     <div className="Home">
 <Link to="/starred"> Go to starred page </Link>
+
+
+<form onSubmit={handleSearch}>
+<input type="text" name="search" placeholder="search" value={search} onChange={handleChange}/>
+<input type="submit" value="submit" />
+</form>
     </div>
     )
 }
